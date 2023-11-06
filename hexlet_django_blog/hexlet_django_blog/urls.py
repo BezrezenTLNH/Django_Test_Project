f866de4724bc63a123afbd41e7024471ccea4775
Mikhail Zherebtsov <bezrezen97@gmail.com>
2023-11-06 21:24:58 +0100

    tryin to change the /articles/ section

--- hexlet_django_blog/hexlet_django_blog/urls.py
+++ hexlet_django_blog/hexlet_django_blog/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from hexlet_django_blog import views
 
+
 urlpatterns = [
-    path('', views.index),
-    path('articles/', include('hexlet_django_blog.article.urls')),
+    path('', views.IndexView.as_view()),
+    path('articles/', include('hexlet_django_blog.articles.urls')),
     path('about/', views.about),
     path('admin/', admin.site.urls),
 ]
